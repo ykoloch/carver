9cf8bd1f7c95c29fa10dddb8a2c7cfb609f71e64
Yurii Kolochkov <me@yurkol.com>
2025-12-12 07:29:47 +0200

    parse command line flags

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const (
+	DEFAULT_OUTPUT_DIR = "./recovered"
+	DEFAULT_WORKERS    = 4
+)
+
+var (
+	device, output string
+	verbose        bool
+	workers        uint
+)
+
 func main() {
-	println("Hello Carver")
+	output = *flag.String("output", DEFAULT_OUTPUT_DIR, fmt.Sprintf("path to the output directory; default is %q", DEFAULT_OUTPUT_DIR))
+	device = *flag.String("device", "", "path to the device to be scanned")
+	if len(device) < 1 {
+		os.Stderr.WriteString("error: the device should be specified")
+		os.Exit(-1)
+	}
+	verbose = *flag.Bool("verbose", false, "verbose mode")
+	workers = *flag.Uint("workers", DEFAULT_WORKERS, "number of workers")
+
+	flag.Parse()
+
+	println(output)
 }
